ec7825fa28cd2e250667dda55c6144941d252bae
Enzo Laert <laertsena@live.co.uk>
2024-09-12 16:32:05 -0300

    inicia menu da aplicação

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-// arrays, objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: false,
-    log: (info) => {
-        console.log(info)
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    
+   while(true){
+    
+        const opcao = await select ({
+            message: "Menu >",
+            choices: [
+            {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+            },
+            {
+              name: "Listar metas",
+              value: "listar"
+            },
+            {
+                name: "Sair",
+                value: "sair"
+            }
+        
+        ]
+        })
+
+        switch(opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                console.log("até a próxima!")
+                return
+        }
+           
+   
+   
+   
+   
     }
-}
+   }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
-// function // arrow function
-// const criarMeta = () => {}
 
-// function criarMeta() {} 
+start()
